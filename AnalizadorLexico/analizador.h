0583ca4d29205cb8ecaa087b73914333ae8c77cf
saucesar <sau.cesarlima2013@gmail.com>
2019-10-02 22:34:54 -0300

    Buscador de links implementado.

---
+++ AnalizadorLexico/analizador.h
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct no{
+  char *link;
+  struct no *proximo;
+}No;
+
+typedef struct listaLinks{
+  No *primeiro;
+  No *ultimo;
+  int quantLinks;
+}ListaLinks;
+
+No* startNo(char *link);
+ListaLinks* startLista();
+void addLista(ListaLinks *lista, char* link);
+char* pop(ListaLinks *lista);
+void print_lista(ListaLinks *lista);
+ListaLinks* buscarLinks(char* arquivo);
+char* pegar_link(FILE *arquivo);
