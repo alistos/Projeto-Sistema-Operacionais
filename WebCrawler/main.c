737b4fd9e791c3f9d3d15ae5eb91aa5d5ce5779e
saucesar <sau.cesarlima2013@gmail.com>
2019-11-12 01:31:40 -0300

    busca para extessão de arquivos.(gif, jpeg, etc..)

--- WebCrawler/main.c
+++ WebCrawler/main.c
@@ -3,16 +3,19 @@
 #include <string.h> //strlen
 #include <sys/socket.h>
 #include "socketCrawler.h"
-#include "analizador.h"
+#include "AnalizadorLexico/analizador.h"
 #include <unistd.h> //close
 #include <netdb.h> //struct addrinfo e função getaddrinfo
 #define LEN_BUFFER 312
 
 int main(int argc, char *argv[]){
+    char *tipo_arquivo = argv[1];
 
-    for(int i = 1 ; i<argc ; i++){
+    for(int i = 2 ; i<argc ; i++){
+        char *dominio = argv[i];
+        Arg_percorrer_dominio *args = start_arg_dominio(dominio,tipo_arquivo);
         pthread_t thread;
-        pthread_create(&thread,NULL,percorrer_dominio,(void*)argv[i]);
+        pthread_create(&thread,NULL,percorrer_dominio,(void*)args);
         pthread_join(thread,NULL);
     }
 
