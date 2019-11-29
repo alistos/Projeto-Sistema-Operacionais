410260b4ddaf5f4498f90dd538bf938c082eebeb
Sau Cesar <sau.cesarlima2013@gmail.com>
2019-11-29 00:40:32 -0300

    Organização dos includes.

--- WebCrawler/socketCrawler.h
+++ WebCrawler/socketCrawler.h
@@ -5,6 +5,10 @@
 #include <stdlib.h>
 #include <string.h> //strlen
 #include <sys/socket.h>
+#include <unistd.h> //close
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/stat.h>
 #include <pthread.h>
 #include <netdb.h> //struct addrinfo e função getaddrinfo
 #include "AnalizadorLexico/analizador.h"
@@ -16,10 +20,18 @@
 #include <openssl/x509.h>
 #include <openssl/x509_vfy.h>
 
+#define RED    "\x1b[31m"
+#define GREEN  "\x1b[32m"
+#define YELLOW "\x1b[33m"
+#define BLUE   "\x1b[34m"
+#define PINK   "\x1b[35m"
+#define RESET  "\x1b[0m"
+
 typedef struct arg_download_pagina{
     char *endereco;
     char *subEndereco;
     char *nome_arquivo_saida;
+    char *extensao_arquivo;
 }Arg_download;
 
 typedef struct arg_percorrer_dominio{
@@ -32,7 +44,7 @@ typedef struct arg_statistica{
     char *tipo_arquivo;
 }Arg_statistica;
 
-Arg_download* start_arg(char *endereco, char *subEndereco, char* nome_arquivo_saida);
+Arg_download* start_arg(char *endereco, char *subEndereco, char* nome_arquivo_saida, char *ext_arq);
 Arg_percorrer_dominio* start_arg_dominio(char *dominio, char *tipo_arquivo);
 Arg_statistica* start_arg_statistica(char *dominio, char *tipo_arquivo);
 struct addrinfo criarServidor(struct addrinfo hints, struct addrinfo **res, char *endereco);
@@ -48,5 +60,5 @@ void *baixar_pagina(void *args);
 void percorrer_links(char* dominio, char* tipo_arquivo);
 int criar_pasta_dominio(char *dominio);
 void *percorrer_dominio(void *dominio);
-void *exibir_statisticas(void *args);
+
 #endif
